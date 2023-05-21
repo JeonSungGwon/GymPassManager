f1fbc516a53ff21cf7788fdd8597a579e0862a4e
SungGwon Jeon <jeonsg9904@gmail.com>
2023-05-22 01:59:08 +0900

    first commit

--- src/main/java/com/example/Toy_project/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/Toy_project/jwt/JwtAccessDeniedHandler.java
@@ -17,6 +17,7 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         // 필요한 권한이 없이 접근하려 할때 403
+        System.out.println("커밋");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
         log.warn("Access Denied: {}", accessDeniedException.getMessage());
     }
